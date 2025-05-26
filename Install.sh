07afe94900fcb58d94b9e7e645ed63ba63f759cb
smith-stack <180924028+smith-stack@users.noreply.github.com>
2025-05-26 14:40:39 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5683,7 +5683,6 @@ function run_option() {
 # 主菜单
 function main_menu() {
 echo "╔════════════════════════════════════════════════════════════════════════╗"
-echo -e "║ ${CYAN}作者${NC}： TinrLin                                                         ║"
 echo -e "║ ${CYAN}脚本快捷方式${NC}： singbox                                                 ║"
 echo -e "║ ${CYAN}Telegram反馈群组${NC}： https://t.me/+EQY_InMZ2L5lODNl                      ║"
 echo -e "║ ${CYAN}项目地址${NC}: https://github.com/smith-stack/sing-box      Version：1.5.0  ║"
